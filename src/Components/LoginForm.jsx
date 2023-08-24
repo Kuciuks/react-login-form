af84b203197f4b4b372281ada38f92309db0a99a
Benas <benas.sarachovas@gmail.com>
2023-08-24 14:13:41 +0100

    added unique ids to input fields

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -11,26 +11,26 @@ export default function LoginForm(){
 
     return(
         <form onSubmit={handleSubmit(submittedData)}>
-            <label htmlFor="username">Username: </label>
-            <input {...register("user_username",{
+            <label htmlFor="username_LOG">Username: </label>
+            <input {...register("user_username_LOG",{
                 pattern: {
                     value: /^[A-Za-z\s]+$/,
                     message: "Using invalid symbols!"
                 },
                 required: "Required field!"
-            })} id="username"></input>
-            <p>{errors.user_username?.message}</p>
+            })} id="username_LOG"></input>
+            <p>{errors.user_username_LOG?.message}</p>
 
-            <label htmlFor="password">Password: </label>
-            <input {...register("user_password", {
+            <label htmlFor="password_LOG">Password: </label>
+            <input {...register("user_password_LOG", {
                 pattern: {
                     value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%^*?&])[A-Za-z\d@$!%^*?&]{8,}$/,
                     message: "The password must contain: at least 8 characters, one lowercase letter, one uppercase letter, one digit, and one special character."
                 },
                 required: "Required field!"
 
-            })} id="password" type="password"></input>
-            <p>{errors.user_password?.message}</p>
+            })} id="password_LOG" type="password"></input>
+            <p>{errors.user_password_LOG?.message}</p>
 
             <button type='submit'>Login</button>
         </form>
--- src/Components/RegisterForm.jsx
+++ src/Components/RegisterForm.jsx
@@ -11,35 +11,35 @@ export default function RegisterForm(){
     return(
         <form onSubmit={handleSubmit(submittedData)}>
 
-            <label htmlFor="username">Username: </label>
-            <input {...register("user_username", {
+            <label htmlFor="username_REG">Username: </label>
+            <input {...register("user_username_REG", {
                 pattern: {
                     value: /^[A-Za-z\s]+$/,
                     message: "Using ivalid symbols"
                 },
                 required: "Required field!"
-            })} id="username"></input>
-            <p>{errors.user_username?.message}</p>
+            })} id="username_REG"></input>
+            <p>{errors.user_username_REG?.message}</p>
 
-            <label htmlFor="email">Email: </label>
-            <input {...register("user_email",{
+            <label htmlFor="email_REG">Email: </label>
+            <input {...register("user_email_REG",{
                 pattern: {
                     value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
                     message: "Using invalid symbols!"
                 },
                 required: "Required field!"
-            })} id="email"></input>
-            <p>{errors.user_email?.message}</p>
+            })} id="email_REG"></input>
+            <p>{errors.user_email_REG?.message}</p>
 
-            <label htmlFor="password">Password: </label>
-            <input {...register("user_password",{
+            <label htmlFor="password_REG">Password: </label>
+            <input {...register("user_password_REG",{
                 pattern: {
                     value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%^*?&])[A-Za-z\d@$!%^*?&]{8,}$/,
                     message: "The password must contain: at least 8 characters, one lowercase letter, one uppercase letter, one digit, and one special character."
                 },
                 required: "Required field!"
-            })} id="password"></input>
-            <p>{errors.user_password?.message}</p>
+            })} id="password_REG"></input>
+            <p>{errors.user_password_REG?.message}</p>
 
             <button type="submit">Register</button>
         </form>
