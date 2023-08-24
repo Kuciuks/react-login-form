9a48913d971f32aa88a30d4783aa741ef75ea41f
Benas <benas.sarachovas@gmail.com>
2023-08-24 16:27:06 +0100

    added user data fetching and validation for login form

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -1,28 +1,41 @@
 import {useForm} from 'react-hook-form'
-
+import {useEffect, useState} from 'react'
 
 export default function LoginForm(){
+
+    const [userData, setUserData] = useState(null)
     
     const {register, handleSubmit, formState : {errors}} = useForm()
 
     const submittedData = (data) => {
-        console.log(data)
+        //console.log("Submitted data: ",data,"\n\n\nFetched user data: ",userData)
+        userData.map((item) =>{
+            data.username_LOG === item.username && data.password_LOG === item.password ? console.log("YOU HAVE LOGGED IN")
+            : console.log("The user does not exist, you should register")
+        })
     }
+
+    useEffect(() => {
+        fetch('https://my-json-server.typicode.com/Kuciuks/react-login-form/users')
+        .then(response => response.json())
+        .then(data => setUserData(data))
+    },[])
+
 
     return(
         <form onSubmit={handleSubmit(submittedData)}>
             <label htmlFor="username_LOG">Username: </label>
-            <input {...register("user_username_LOG",{
+            <input {...register("username_LOG",{
                 pattern: {
                     value: /^[A-Za-z\s]+$/,
                     message: "Using invalid symbols!"
                 },
                 required: "Required field!"
             })} id="username_LOG"></input>
-            <p>{errors.user_username_LOG?.message}</p>
+            <p>{errors.username_LOG?.message}</p>
 
             <label htmlFor="password_LOG">Password: </label>
-            <input {...register("user_password_LOG", {
+            <input {...register("password_LOG", {
                 pattern: {
                     value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%^*?&])[A-Za-z\d@$!%^*?&]{8,}$/,
                     message: "The password must contain: at least 8 characters, one lowercase letter, one uppercase letter, one digit, and one special character."
@@ -30,7 +43,7 @@ export default function LoginForm(){
                 required: "Required field!"
 
             })} id="password_LOG" type="password"></input>
-            <p>{errors.user_password_LOG?.message}</p>
+            <p>{errors.password_LOG?.message}</p>
 
             <button type='submit'>Login</button>
         </form>
--- src/Components/RegisterForm.jsx
+++ src/Components/RegisterForm.jsx
@@ -12,34 +12,34 @@ export default function RegisterForm(){
         <form onSubmit={handleSubmit(submittedData)}>
 
             <label htmlFor="username_REG">Username: </label>
-            <input {...register("user_username_REG", {
+            <input {...register("username_REG", {
                 pattern: {
                     value: /^[A-Za-z\s]+$/,
                     message: "Using ivalid symbols"
                 },
                 required: "Required field!"
             })} id="username_REG"></input>
-            <p>{errors.user_username_REG?.message}</p>
+            <p>{errors.username_REG?.message}</p>
 
             <label htmlFor="email_REG">Email: </label>
-            <input {...register("user_email_REG",{
+            <input {...register("email_REG",{
                 pattern: {
                     value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
                     message: "Using invalid symbols!"
                 },
                 required: "Required field!"
             })} id="email_REG"></input>
-            <p>{errors.user_email_REG?.message}</p>
+            <p>{errors.email_REG?.message}</p>
 
             <label htmlFor="password_REG">Password: </label>
-            <input {...register("user_password_REG",{
+            <input {...register("password_REG",{
                 pattern: {
                     value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%^*?&])[A-Za-z\d@$!%^*?&]{8,}$/,
                     message: "The password must contain: at least 8 characters, one lowercase letter, one uppercase letter, one digit, and one special character."
                 },
                 required: "Required field!"
             })} id="password_REG"></input>
-            <p>{errors.user_password_REG?.message}</p>
+            <p>{errors.password_REG?.message}</p>
 
             <button type="submit">Register</button>
         </form>
