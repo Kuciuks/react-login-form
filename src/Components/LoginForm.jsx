a6de637c1d4426bb55d59c0a56f45bc021b9bb9f
Benas <benas.sarachovas@gmail.com>
2023-08-24 17:03:05 +0100

    updated db.json for validation testing, displaying login status

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -4,15 +4,18 @@ import {useEffect, useState} from 'react'
 export default function LoginForm(){
 
     const [userData, setUserData] = useState(null)
+    const [loginStatus, setLoginStatus] = useState(null)
     
     const {register, handleSubmit, formState : {errors}} = useForm()
 
     const submittedData = (data) => {
         //console.log("Submitted data: ",data,"\n\n\nFetched user data: ",userData)
         userData.map((item) =>{
-            data.username_LOG === item.username && data.password_LOG === item.password ? console.log("YOU HAVE LOGGED IN")
-            : console.log("The user does not exist, you should register")
+            data.username_LOG === item.username && data.password_LOG === item.password ? setLoginStatus("You have successfully logged in!")
+            : setLoginStatus("There's no such user, please register instead")
         })
+
+        
     }
 
     useEffect(() => {
@@ -23,29 +26,32 @@ export default function LoginForm(){
 
 
     return(
-        <form onSubmit={handleSubmit(submittedData)}>
-            <label htmlFor="username_LOG">Username: </label>
-            <input {...register("username_LOG",{
-                pattern: {
-                    value: /^[A-Za-z\s]+$/,
-                    message: "Using invalid symbols!"
-                },
-                required: "Required field!"
-            })} id="username_LOG"></input>
-            <p>{errors.username_LOG?.message}</p>
+        <div>    
+            <form onSubmit={handleSubmit(submittedData)}>
+                <label htmlFor="username_LOG">Username: </label>
+                <input {...register("username_LOG",{
+                    pattern: {
+                        value: /^[A-Za-z\s]+$/,
+                        message: "Using invalid symbols!"
+                    },
+                    required: "Required field!"
+                })} id="username_LOG"></input>
+                <p>{errors.username_LOG?.message}</p>
 
-            <label htmlFor="password_LOG">Password: </label>
-            <input {...register("password_LOG", {
-                pattern: {
-                    value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%^*?&])[A-Za-z\d@$!%^*?&]{8,}$/,
-                    message: "The password must contain: at least 8 characters, one lowercase letter, one uppercase letter, one digit, and one special character."
-                },
-                required: "Required field!"
+                <label htmlFor="password_LOG">Password: </label>
+                <input {...register("password_LOG", {
+                    pattern: {
+                        value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%^*?&])[A-Za-z\d@$!%^*?&]{8,}$/,
+                        message: "The password must contain: at least 8 characters, one lowercase letter, one uppercase letter, one digit, and one special character."
+                    },
+                    required: "Required field!"
 
-            })} id="password_LOG" type="password"></input>
-            <p>{errors.password_LOG?.message}</p>
+                })} id="password_LOG" type="password"></input>
+                <p>{errors.password_LOG?.message}</p>
 
-            <button type='submit'>Login</button>
-        </form>
+                <button type='submit'>Login</button>
+            </form>
+            <p>{loginStatus}</p>
+        </div>
     )
 }
